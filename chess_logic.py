b25fc429fd0347ab0c51ddf190fcd7a451803332
Mxsxll <coconraupe@gmail.com>
2025-01-31 18:05:09 +0100

    i have a plan

--- chess_logic.py
+++ chess_logic.py
@@ -5,9 +5,11 @@ bauer = solid.import_scad("bauer.scad")
 board = solid.import_scad("Schachboard.scad")
 white_p = bauer.white_pawn()
 board_main = board.checkerboard(7, 7, True)
-board1 = solid.translate([40, 40, 3])
-white_p_sized = solid.resize([9, 9, 14])(white_p)
+board1 = solid.translate([-40, -40, -1.5])(board_main)
+white_p_sized = solid.translate([-5, -5, 1.5])(solid.resize([9, 9, 14])(white_p)) 
 
-solid.scad_render_to_file(white_p_sized,  out)
-solid.scad_render_to_file(board1, out)
+scene = white_p_sized + board1
+
+solid.scad_render_to_file(scene, out)
+
 
